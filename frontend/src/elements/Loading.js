248e792055486a38335635f7c80cf2f41718694e
iamjacek <iamjacek@icloud.com>
2021-11-24 23:31:28 +0000

    fetch products from API, add loader from react-spinners-css

---
+++ frontend/src/elements/Loading.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Loader from "@bit/joshk.react-spinners-css.facebook";
+
+export default function Loading() {
+  return (
+    <div className="smalll">
+      <Loader color="#805ad5" size={35} />
+    </div>
+  );
+}
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,14 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import Product from "../elements/Product";
-import data from "../data";
+import Loading from "../elements/Loading";
 
 export default function HomeScreen() {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        setLoading(false);
+        const { data } = await axios.get(`/api/products`);
+        setProducts(data);
+      } catch (error) {
+        setLoading(false);
+        setError(error);
+      }
+    };
+    fetchProducts();
+  }, []);
   return (
     <>
-      <div className="home__container">
-        {data.products.map((product) => (
-          <Product key={product._id} product={product} />
-        ))}
+      <div>
+        {loading ? (
+          <Loading />
+        ) : error ? (
+          <div>{error}</div>
+        ) : (
+          <div className="home__container">
+            {products.map((product) => (
+              <Product key={product._id} product={product} />
+            ))}
+          </div>
+        )}
       </div>
     </>
   );
