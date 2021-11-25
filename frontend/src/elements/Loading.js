b8d263d16c309e9302bdfef5088ec4dd16dfb280
iamjacek <iamjacek@icloud.com>
2021-11-25 10:58:27 +0000

    edit Loading element

--- frontend/src/elements/Loading.js
+++ frontend/src/elements/Loading.js
@@ -3,7 +3,7 @@ import Loader from "@bit/joshk.react-spinners-css.facebook";
 
 export default function Loading() {
   return (
-    <div className="smalll">
+    <div className="loading">
       <Loader color="#805ad5" size={35} />
     </div>
   );
